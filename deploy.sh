11f44532d1f70967500745f6f77ae07debc38f9e
raghudevops21 <34628005+raghudevops21@users.noreply.github.com>
2018-10-13 17:34:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 TOMCAT_HOST=172.17.0.3
 TOMCAT_USER=root
+TOMCAT_PWD=root
 TOMCAT_HOME=/usr/local/tomcat
 #
 WAR_FILE_PATH=/home/interview/.jenkins/workspace/job2/target/mavennew.war
@@ -12,7 +13,7 @@ $TOMCAT_HOME/bin/shutdown.sh 1>/dev/null 2>&1
 if [ -f $WAR_FILE_PATH ];then
     echo "deploying mavennew application..."
     sleep 4
-    ssh $TOMCAT_USER@$TOMCAT_HOST
+    sshpass -p $TOMCAT_PWD ssh $TOMCAT_USER@$TOMCAT_HOST
     scp $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapps
 else
    echo "war file doesn't exist! Please check the build"
