a3f684a5174f3d9725c277a6cb56c1ea8528c21f
raghudevops21 <34628005+raghudevops21@users.noreply.github.com>
2018-10-13 16:07:48 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -10,7 +10,7 @@ do
     echo "=============================="
     echo "Creating system$i .."
     sleep 1
-    docker run --name newsystem$i -d -it raghudevops21/nginx.img /bin/bash
+    sudo docker run -it tomcat /bin/bash
     echo "system$i has been created!"
     echo "=============================="
 done
